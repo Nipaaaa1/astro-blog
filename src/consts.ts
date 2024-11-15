c16aa4377e1bada9e470ed7f6c9d3abdfdf2d2f7
Nipaaaa1 <frostone224@gmail.com>
2024-11-15 07:24:26 +0700

    chore: ubah konten yg ada di halaman depan

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "SYD Blog",
+  EMAIL: "devansayidina42@gmail.rom",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
@@ -35,7 +35,7 @@ export const SOCIALS: Socials = [
   },
   { 
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/Nipaaaa1"
   },
   { 
     NAME: "linkedin",

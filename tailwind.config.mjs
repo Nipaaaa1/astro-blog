2fab0380a831478b9db66be756c7a9906e53d826
Nipaaaa1 <frostone224@gmail.com>
2024-12-05 22:06:57 +0700

    refactor: tambahin color palet buat design system baru

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,6 +13,20 @@ export default {
         serif: ["Lora", ...defaultTheme.fontFamily.serif],
         title: ["BoldGroove", ...defaultTheme.fontFamily.serif]
       },
+      colors: {
+        primary: '#FFA89A',
+        secondary: '#ABE5BC',
+        accent: {
+          yellow: '#FFD787',
+          violet: '#CFADE8',
+          orange: '#FFA776',
+        },
+        neutral: {
+          white: '#FFFAF0',
+          gray: '#505050',
+          black: '#1E1E1E',
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
